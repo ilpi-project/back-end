1a28697cc294cf24b9d14a2be5c88a388e9744d9
marcellacarneiro <carneiromarcella53@gmail.com>
2025-04-30 22:02:25 -0300

    adiciona rota useRoutes ao app.js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
+import dotenv from 'dotenv';
+import express, { json } from 'express';
+import cors from 'cors';
+import connectDB from './config/db.js';
 
+import userRoutes from './routes/userRoutes.js';
+
+dotenv.config();
 const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
+
+app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
