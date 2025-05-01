28923170aaad3179719ac3e22b3020e8286bb387
marcellacarneiro <carneiromarcella53@gmail.com>
2025-04-30 21:59:53 -0300

    cria model User

---
+++ models/User.js
@@ -0,0 +1,31 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    cpf: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    birthdate: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+export default model('User', userSchema);
