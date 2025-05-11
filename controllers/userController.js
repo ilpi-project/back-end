237f1d5aa6083212791de40046b05da78b86b0ab
marcellacarneiro <carneiromarcella53@gmail.com>
2025-05-10 21:00:28 -0300

    adiciona rotas de autenticação

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,42 @@ import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
+export const validateUser = async (req, res) => {
+    const token = req.headers["authorization"]?.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Token não fornecido" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+            return res.status(404).json({ message: "Usuário não encontrado" });
+        }
+        res.status(200).json({ name: user.name });
+    } catch (e) {
+        return res.status(401).json({ message: "Token inválido ou expirado" });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(400).json({ message: "Credenciais inválidas" });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "12h",
+        });
+        res.json({ userId: user._id, token });
+    } catch (e) {
+        res.status(500).json({ message: "Erro ao realizar login: ", e });
+    }
+};
+
 export const getUserById = async (req, res) => {
     const { id } = req.params;
     try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getUsers, getUserById, createUser, updateUser, deleteUser } from '../controllers/userController.js';
+import { getUsers, getUserById, createUser, updateUser, deleteUser, login, validateUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
+router.post('/login', login);
+router.get('/validate-user', validateUser);
 router.get('/', getUsers);
 router.get('/:id', getUserById);
 router.post('/create', createUser);
